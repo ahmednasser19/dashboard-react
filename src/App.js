bb0cc3a43cb95d1a2eb7420057882f2d083ac97a
ahmed nasser <ahmednas197@gmail.com>
2022-05-21 12:44:11 +0200

    Add basic routes

--- src/App.js
+++ src/App.js
@@ -22,10 +22,50 @@ const App = () => {
                             </button>
                         </TooltipComponent>
                     </div>
-                    {activeMenu ? (<div>sidebar</div>) : (<div></div>)}
+                    {activeMenu ? (<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white' >sidebar</div>) : (<div className='w-0 dark:bg-secondary-dark-bg'>sidebar w-0</div>)}
+                    <div className={
+                        `dark:bg-main-bg bg-main-bg min-h-screen  w-full ${activeMenu ?
+                            'md:ml-72 '
+                            :
+                            'flex-2'}`
+                    }>
+                        <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+
+                        </div>
+
+                    </div>
+                    {/* main div for Routing  */}
+                    <div>
+                        <Routes>
+                            {/* Dashboard */}
+                            <Route path="/" element="ECommerce" />
+                            <Route path="/ecommerce" element="ECommerce" />
+                            {/* pages */}
+                            <Route path='/orders' element="Orders" />
+                            <Route path='/employee' element="Employee" />
+                            <Route path='/Customers' element="customers" />
+
+                            {/* apps  */}
+                            <Route path="/kanban" element="Kanban" />
+                            <Route path="/editor" element="Editor" />
+                            <Route path="/calendar" element="Calendar" />
+                            <Route path="/color-picker" element="ColorPicker" />
+
+                            {/* charts  */}
+                            <Route path="/line" element="Line" />
+                            <Route path="/area" element="Area" />
+                            <Route path="/bar" element="Bar" />
+                            <Route path="/pie" element="Pie" />
+                            <Route path="/financial" element="Financial" />
+                            <Route path="/color-mapping" element="ColorMapping" />
+                            <Route path="/pyramid" element="Pyramid" />
+                            <Route path="/stacked" element="Stacked" />
+
+                        </Routes>
+                    </div>
                 </div>
-            </BrowserRouter>
-        </div>
+            </BrowserRouter >
+        </div >
     )
 }
 
